9b2a677fc949834ae02003a721e2e89509e07933
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-04-07 17:25:26 -0500

    added todoSlice/working

---
+++ src/features/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    items: []
+}
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            return {items: [...state.items, action.payload]}
+        }
+    }
+})
+
+export const { addTodo } = todoSlice.actions
+export default todoSlice.reducer
